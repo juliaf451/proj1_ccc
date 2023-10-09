662cf6616ad1ff58d9c8d8db471ac4c34865c7e0
juliaf451 <97061487+juliaf451@users.noreply.github.com>
2023-10-09 04:17:10 +0000

    fixed audit

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/admin",
@@ -14,6 +16,16 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = 100"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_ml = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_potions = 0"))
+
     return "OK"
 
 
@@ -23,7 +35,7 @@ def get_shop_info():
 
     # TODO: Change me!
     return {
-        "shop_name": "Potion Shop",
-        "shop_owner": "Potion Seller",
+        "shop_name": "Cal Poly Concoctions",
+        "shop_owner": "Julia",
     }
 
--- src/api/audit.py
+++ src/api/audit.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
 import math
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/audit",
@@ -12,8 +14,14 @@ router = APIRouter(
 @router.get("/inventory")
 def get_inventory():
     """ """
-    
-    return {"number_of_potions": 0, "ml_in_barrels": 0, "gold": 0}
+    with db.engine.begin() as connection:
+        inventory = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).first()
+        potions = inventory.num_red_potions + inventory.num_blue_potions + inventory.num_green_potions
+        ml = inventory.num_red_ml + inventory.num_blue_ml + inventory.num_green_ml
+        gold = inventory.gold
+
+
+    return {"number_of_potions": potions, "ml_in_barrels": ml, "gold": gold}
 
 class Result(BaseModel):
     gold_match: bool
